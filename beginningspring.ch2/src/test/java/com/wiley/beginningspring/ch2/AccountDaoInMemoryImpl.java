0451443b6644bcf3c9c0e58c252f3afbc8663ac4
Your Name <ali.alabbasi03@gmail.com>
2021-04-18 19:47:41 -0400

    update #1

--- beginningspring.ch2/src/test/java/com/wiley/beginningspring/ch2/AccountDaoInMemoryImpl.java
+++ beginningspring.ch2/src/test/java/com/wiley/beginningspring/ch2/AccountDaoInMemoryImpl.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AccountDaoInMemoryImpl implements AccountDao {
 	
 	private Map<Long,Account> accountsMap = new HashMap<>();
